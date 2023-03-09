4c46f3f5db82ee1f559c0d6daf67eeee68113375
Areel Khan <you@example.com>
2023-03-08 21:17:53 -0500

    added correct version of datamodel

--- datamodel.py
+++ datamodel.py
@@ -31,35 +31,38 @@ class Order:
     
 
 class OrderDepth:
-    def __init__(self):
+    def __init__(self, buy_orders: Dict[int, int], sell_orders: Dict[int, int]):
         self.buy_orders: Dict[int, int] = {}
         self.sell_orders: Dict[int, int] = {}
 
 
 class Trade:
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = "", seller: UserId = "") -> None:
+    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None, timestamp: int = 0) -> None:
         self.symbol = symbol
         self.price: int = price
         self.quantity: int = quantity
         self.buyer = buyer
         self.seller = seller
+        self.timestamp = timestamp
 
     def __str__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
 
     def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
 
-
+    
 class TradingState(object):
-    def __init__(self,
-                 timestamp: Time,
-                 listings: Dict[Symbol, Listing],
-                 order_depths: Dict[Symbol, OrderDepth],
-                 own_trades: Dict[Symbol, List[Trade]],
-                 market_trades: Dict[Symbol, List[Trade]],
-                 position: Dict[Product, Position],
-                 observations: Dict[Product, Observation]):
+    def __init__(
+            self,
+            timestamp: Time,
+            listings: Dict[Symbol, Listing],
+            order_depths: Dict[Symbol, OrderDepth],
+            own_trades: Dict[Symbol, List[Trade]],
+            market_trades: Dict[Symbol, List[Trade]],
+            position: Dict[Product, Position],
+            observations: Dict[Product, Observation]
+        ):
         self.timestamp = timestamp
         self.listings = listings
         self.order_depths = order_depths
