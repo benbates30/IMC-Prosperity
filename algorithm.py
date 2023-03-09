0b524ead0dae67b8983b44311927df0c430ca410
Areel Khan <you@example.com>
2023-03-09 00:57:01 -0500

    added trading algorithm

---
+++ algorithm.py
@@ -0,0 +1,43 @@
+import json
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+
+        print("\n\n######################################")
+        json_state = json.dumps(state, default=lambda o: o.__dict__, indent=4)
+        print(json_state)
+        print("######################################\n\n")
+
+        result = {}
+
+        for product in state.order_depths.keys():
+
+            if product == 'PEARLS':
+
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: list[Order] = []
+                acceptable_price = 10000
+
+                if len(order_depth.sell_orders) > 0:
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_ask_volume = order_depth.sell_orders[best_ask]
+                    if best_ask < acceptable_price:
+                        print("BUY", str(-best_ask_volume) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_volume))
+
+                if len(order_depth.buy_orders) != 0:
+                    best_bid = max(order_depth.buy_orders.keys())
+                    best_bid_volume = order_depth.buy_orders[best_bid]
+                    if best_bid > acceptable_price:
+                        print("SELL", str(best_bid_volume) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_volume))
+                result[product] = orders
+        return result
